38048f83d32b6d92268bfb346d0e49b28569e0a1
jamesrhee91 <jamesshrhee@gmail.com>
2017-07-13 10:35:03 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,48 @@
 
+def sort_array_asc(arr)
+  arr.sort
+end
+
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by {|e| e.length}
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+  result = []
+  arr.each do |e|
+    e[2] = "$"
+    result << e
+  end
+  result
+end
+
+def find_a(arr)
+  arr.select {|e| e.start_with?("a")}
+end
+
+def sum_array(arr)
+  arr.reduce(:+)
+end
+
+def add_s(arr)
+  arr.map.with_index do |e, i|
+    if i == 1
+      e
+    else
+      e << "s"
+    end
+  end
+end
